7f209ded7df2026dcf80612e335f9b044410adab
Igzinx7 <igor.cordeiro.martins@escola.pr.gov.br>
2024-10-07 10:39:25 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  background("white");
+}
+
+function draw() {
+  
+  stroke ("blue");
+  fill ("red");
+  
+  console.log(mouseIsPressed);
+  rect(mouseX, mouseY, 100, 150);
+}
